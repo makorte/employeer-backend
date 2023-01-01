60c56c8a074af10ef87c3cb1a58b6f7b960a961a
Maximilian Korte <maximilian.korte@adesso.de>
2023-01-01 01:16:14 +0100

    Configure database

--- adapter/build.gradle.kts
+++ adapter/build.gradle.kts
@@ -1,8 +1,15 @@
 plugins {
     id("java-config")
+    id("io.spring.dependency-management") version "1.1.0"
 }
 
 dependencies {
     implementation(project(":service"))
     implementation(project(":configuration"))
+    implementation("org.postgresql:postgresql:42.5.1")
+    implementation("org.springframework.data:spring-data-jpa")
+
+}
+repositories {
+    mavenCentral()
 }
