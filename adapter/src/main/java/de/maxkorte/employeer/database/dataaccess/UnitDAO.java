3b233ce0b789c5c20005fa357fc7e4df97911d4d
Maximilian Korte <maximilian.korte@adesso.de>
2023-01-06 10:08:12 +0100

    Add spring repository annotation to unit dao

--- adapter/src/main/java/de/maxkorte/employeer/database/dataaccess/UnitDAO.java
+++ adapter/src/main/java/de/maxkorte/employeer/database/dataaccess/UnitDAO.java
@@ -1,4 +1,11 @@
 package de.maxkorte.employeer.database.dataaccess;
 
-public interface UnitDAO {
+import de.maxkorte.employeer.database.datatransfer.UnitDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UnitDAO extends JpaRepository<UnitDTO, UUID> {
 }
