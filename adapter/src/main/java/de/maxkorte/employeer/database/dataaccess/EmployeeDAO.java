a10afb0135137142ae8188e332de1dc5d89b0bd0
Maximilian Korte <maximilian.korte@adesso.de>
2023-01-06 10:19:34 +0100

    Make employee dao use dto

--- adapter/src/main/java/de/maxkorte/employeer/database/dataaccess/EmployeeDAO.java
+++ adapter/src/main/java/de/maxkorte/employeer/database/dataaccess/EmployeeDAO.java
@@ -1,12 +1,13 @@
 package de.maxkorte.employeer.database.dataaccess;
 
 import de.maxkorte.employeer.Employee;
+import de.maxkorte.employeer.database.datatransfer.EmployeeDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface EmployeeDAO extends JpaRepository<Employee, UUID> {
+public interface EmployeeDAO extends JpaRepository<EmployeeDTO, UUID> {
 
 }
