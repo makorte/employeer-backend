535b15d33f36ed5b80af5df434d769124d042f56
Maximilian Korte <maximilian.korte@adesso.de>
2023-01-23 19:24:15 +0100

    Add swagger configuration

--- adapter/build.gradle.kts
+++ adapter/build.gradle.kts
@@ -8,4 +8,6 @@ dependencies {
     implementation("org.postgresql:postgresql:42.5.1")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
+    implementation("org.hibernate.validator:hibernate-validator:8.0.0.Final")
 }
